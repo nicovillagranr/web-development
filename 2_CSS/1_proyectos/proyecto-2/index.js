2c9b8621b2f8fbca8e8b8f76b381a7f80cdc8e7f
Nico Villagran <nicovillagranroses@gmail.com>
2025-10-24 11:02:27 -0300

    Actualizando mi folder de CSS

--- 2_CSS/1_proyectos/proyecto-2/index.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load' , () => {
-    const s = document.querySelector(".wrap__gear")
-    gsap.to(s ,  {
-        rotation: 360,
-        duration: 2,
-        repeat: -1,
-        ease: "none",
-    })
-})
---
+++ 2_CSS/bem.md
@@ -0,0 +1,11 @@
+<!-- 
+
+Bem = Bloque Elemento Modificador
+
+Se usa siempre CLASS no ID
+Class = Aplicar Estilos
+ID = Aplicar JavaSript
+
+
+
+ -->
