311033061dad890409f18c01be590dd7af8cfe35
Nico Villagran <nicovillagranroses@gmail.com>
2025-11-25 14:51:51 -0300

    Actualizando repositorio Grid CSS

---
+++ 01-fundamentals/02-css/02-layouts/2-grid/projects/proyecto-1/src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <ul className="footer__list">
+                <li className="footer__list-item"><a href="#" className="footer__list-link">About Us</a></li>
+                <li className="footer__list-item"><a href="#" className="">Privacy Policy</a></li>
+                <li className="footer__list-item"><a href="#" className="">Terms of Service</a></li>
+                <li className="footer__list-item"><a href="#" className="">XML Sitemap</a></li>
+                <li className="footer__list-item"><a href="#" className="">Contact</a></li>
+            </ul>
+        </footer>
+    )
+}
+export default Footer
