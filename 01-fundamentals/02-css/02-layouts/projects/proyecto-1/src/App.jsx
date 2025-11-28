ae36d5f07665c7a15cef72cff00b6287f600d1c1
Nico Villagran <nicovillagranroses@gmail.com>
2025-11-28 11:38:20 -0300

    Actualización de repositorio para terminar Noviembre

---
+++ 01-fundamentals/02-css/02-layouts/projects/proyecto-1/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Components/Header/Header"
+import Main from "./Components/Main/Main"
+import Footer from "./Components/Footer/Footer"
+
+import "./reset.css"
+import "./assets/Baloo_2/font.css"
+import './App.css'
+import "./assets/fontawesome/css/all.min.css"
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+export default App
---
+++ 01-fundamentals/02-css/02-layouts/projects/proyecto-4/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+import "./reset.css"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Proyecto 4</h1>
+    </>
+  )
+}
+export default App
