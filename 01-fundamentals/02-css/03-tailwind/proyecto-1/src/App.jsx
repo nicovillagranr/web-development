3e36eca2affd24826dd2972616c234b96af7d4dc
Nico Villagran <nicovillagranroses@gmail.com>
2025-12-16 23:37:33 -0300

    refactor: Iniciando primer proyecto con Talwind. Aprendiendo clases y maquetando mi primer tamplate con React

---
+++ 01-fundamentals/02-css/03-tailwind/proyecto-1/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/1-Header/Header"
+
+
+
+import './App.css'
+import "./assets/styles/reset.css"
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+export default App
+
---
+++ 01-fundamentals/02-css/03-tailwind/proyecto-1/tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        screens: {
+            xs: "360px",
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
+        },
+    },
+    plugins: [],
+}
